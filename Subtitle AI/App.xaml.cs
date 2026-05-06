fdd580879f0faedd2f7bbe4b800c7a6f2483a3ce
Hasan Akgun <akgunhasan8@gmail.com>
2026-05-06 23:13:33 +0300

    Add project files.

---
+++ Subtitle AI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Subtitle_AI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
